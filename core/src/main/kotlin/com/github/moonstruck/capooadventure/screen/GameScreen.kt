4ec22a6f82761d825a25020a1af6da75643ed2dc
moonstruck19 <hungnp.21it@vku.udn.vn>
2023-04-07 10:37:29 +0700

    second commit

--- core/src/main/kotlin/com/github/moonstruck/capooadventure/screen/GameScreen.kt
+++ core/src/main/kotlin/com/github/moonstruck/capooadventure/screen/GameScreen.kt
@@ -2,32 +2,45 @@ package com.github.moonstruck.capooadventure.screen
 
 
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Batch
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 import ktx.app.KtxScreen
-import ktx.graphics.use
 import ktx.log.logger
 
 class GameScreen : KtxScreen {
-    private val spriteBatch : Batch = SpriteBatch()
-    private val stage : Stage = Stage()
+    private val stage : Stage = Stage(ExtendViewport(16f, 9f))
     private val texture : Texture = Texture(
         "move.png"
     )
-    private val background : Texture = Texture(
-        "fck.png"
-    )
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
 
     override fun show() {
         log.debug { "GameScreen get shown" }
+        stage.addActor(
+            Image(texture).apply {
+                setPosition(3f, 3f)
+                setSize(3f, 3f)
+                setScaling(Scaling.fill)
+            }
+        )
     }
     override fun render(delta: Float) {
-        spriteBatch.use {
-            it.draw(texture,0f,0f)
-            it.draw(background, 340f,340f)
+        with(stage) {
+            act(delta)
+            draw()
         }
     }
+
+    override fun dispose() {
+        stage.dispose()
+        texture.dispose()
+    }
+
     companion object {
         private val log = logger<GameScreen>()
     }
